4c0fbbf9d9ed558b781964aefb38c6cbb6b2b261
quinnschwartz <quinnschwartz@gmail.com>
2017-01-06 12:51:47 -0800

    fix syntax errors in forEach loop.  application now works.

--- build/js/app.js
+++ build/js/app.js
@@ -11,9 +11,9 @@ Doctor = function(medicalIssue) {
 
 
 Doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
-     $('.showDoctors').text(result.datas.forEach(function(data) {
-       $('.showDoctors').append("<li>" + "<span>Name:</span>" + " " + data.practice.name + "</li>");
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=' + apiKey).then(function(result) {
+     $('.showDoctors').text(result.data.forEach(function(data) {
+       $('.showDoctors').append("<li>" + "<span>Name:</span>" + " " + data.profile.bio + "</li>");
       console.log(result);
      }));
   })
