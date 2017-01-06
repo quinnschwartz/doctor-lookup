dc236c12c820c12a57eedfc161192b4d898b9cfb
quinnschwartz <quinnschwartz@gmail.com>
2017-01-06 10:35:39 -0800

    require API key in doctor.js file.

--- js/doctor.js
+++ js/doctor.js
@@ -1,10 +1,13 @@
+var apiKey = require('./../.env').apiKey;
+
+
 Doctor = function(medicalIssue) {
   this.medicalIssue = medicalIssue;
 };
 
 
-doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+Doctor.prototype.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + exports.apiKey)
    .then(function(result) {
       console.log(result);
     })
